46e51c61e52c95be544ac61e8fca5a68420ff59a
Kevin Delvo <kevinbdelvo@gmail.com>
2018-02-06 07:50:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -70,5 +70,13 @@ function removeFromCart(name) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (!cardNumber) {
+    return console.log("Sorry, we don't have a credit card on file for you.");
+  } else {
+    console.log(`Your total cost is ${$total()}, which will be charged to the card ${number}.`);
+    cart = [];
+  }
 }
+
+
+
