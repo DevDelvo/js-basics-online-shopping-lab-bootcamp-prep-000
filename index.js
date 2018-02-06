a311aa0ca647219063a5477799bf8ecbb3aa78d6
Kevin Delvo <kevinbdelvo@gmail.com>
2018-02-06 07:14:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function viewCart() {
   if (cart.length === 0) {
     return "Your shopping cart is empty.";
   } else if (cart.length === 1) {
-    return `In your cart, you have Object.keys()
+    return `In your cart, you have ${Object.keys(cart[0])[0]} at $${Object.values(cart[0])[0]}.`
   }
 }
 
