0159b2f3177dc1c856bfd106e066802d63daaa4f
Kevin Delvo <kevinbdelvo@gmail.com>
2018-02-06 07:12:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function viewCart() {
   if (cart.length === 0) {
     return "Your shopping cart is empty.";
   } else if (cart.length === 1) {
-    return `In your cart, you have 
+    return `In your cart, you have Object.keys()
   }
 }
 
