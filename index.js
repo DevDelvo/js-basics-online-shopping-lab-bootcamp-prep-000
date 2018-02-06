b4609f67233d2fa01c07f9c8159b4e5048281980
Kevin Delvo <kevinbdelvo@gmail.com>
2018-02-06 07:25:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,9 +28,9 @@ function viewCart() {
     view += `${Object.keys(cart[0])[0]} at $${Object.values(cart[0])[0]} and ${Object.keys(cart[1])[0]} at $${Object.values(cart[1])[0]}.`; 
   } else {
       for (var i = 0; i < clength; i++) {
-        index = cart[i];
-        item = Object.keys(index)[0];
-        price = Object.values(index)[0];
+        var index = cart[i];
+        var item = Object.keys(index)[0];
+        var price = Object.values(index)[0];
         
         if (i === clength - 1) {
           view += `and ${item} at $${price}.`;
