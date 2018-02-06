a26933b2c019cb2caae0241ca1c6803c2f6cf2fb
Kevin Delvo <kevinbdelvo@gmail.com>
2018-02-06 07:51:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ function placeOrder(cardNumber) {
   if (!cardNumber) {
     return console.log("Sorry, we don't have a credit card on file for you.");
   } else {
-    console.log(`Your total cost is ${$total()}, which will be charged to the card ${number}.`);
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${number}.`);
     cart = [];
   }
 }
